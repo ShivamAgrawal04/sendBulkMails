0fe09e083284e768e3fdd9333dd3f943c9b8175b
ShivamAgrawal04 <shivamagrawal0455@gmail.com>
2026-04-26 23:17:51 +0530

    update url in frontend redux/emailslice.js

--- frontend/src/redux/emailSlice.js
+++ frontend/src/redux/emailSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const API_URL = "http://localhost:4000/api/email";
+const API_URL = `${import.meta.env.VITE_API_URL}/api/auth`;
 
 export const sendEmail = createAsyncThunk(
   "email/sendEmail",
